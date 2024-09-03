3020756a9b9e9b146a4b3d1c331ee4a17bff0b8d
“Mishel <rmishel564@gmail.com>
2024-09-03 07:45:32 -0500

    se crea proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Link,Route,Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 
 function App() {
 
   return (
-   <>
-   <Link to="/"> Home</Link>
-   <Link to="/Login"> Login</Link>
-   <Routes>
-<Route path="/" element={<Home/>}>
-</Route>
-<Route path="/Login" element={<Login/>}>
-</Route>
-   </Routes>
-   </>
-    
+    <>
+      <Link to="/"> Home</Link>
+      <Link to="/Login"> Login</Link>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/Login" element={<Login />}>
+        </Route>
+      </Routes>
+    </>
+
   )
 }
 
